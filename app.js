17ae35a46cea854a1459e7f58b75cc5f68a2b7ac
Nate Travis <nathaniel@cmu-902222.wv.cc.cmu.edu>
2015-02-07 15:10:26 -0500

    port

--- app.js
+++ app.js
@@ -233,7 +233,7 @@ var convertTime = function(time) {
 	return querystring.escape(d+" "+t);
 }
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
 	var host = server.address().address;
 	var port = server.address().port;
