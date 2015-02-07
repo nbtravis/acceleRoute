9aaf3daa0455ac110c69e9af8a9bf8ed83345b68
Ajay Ravindran <ajax013@gmail.com>
2015-02-07 15:09:30 -0500

    getting close

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/', function (req, res) {
     	speed = req.query.speed;
 
     calculate(from, to, time, speed, res);
+
 });
 var memoTime = {};
 var memoPrev = {};
@@ -25,6 +26,7 @@ var memoDir = {};
 var end = "";
 var stack = [];
 var accumulatePath = function() {
+	if (!(end in memoTime)) return [];
 	var result = [];
 	var curr = end;
 	while (memoPrev[curr] !== "") {
@@ -66,8 +68,8 @@ var calculate = function(from, to, time, speed, res) {
 				var src = instr.substring(j, i-3);
 
 				var scaledTime = duration / speed;
-				var newDepartureTime = time + scaledTime*1000;
-				memoTime[src] = time;
+				var newDepartureTime = (new Date(Date(time))).getTime() + scaledTime*1000;
+				memoTime[src] = (new Date(Date(time))).getTime();
 				memoPrev[src] = "";
 				memoDir[src] = "";
 
@@ -119,13 +121,24 @@ var calculateHelper = function(to, speed, res) {
 	console.log(stack);
 	var pos = stack.pop();
 	var time = pos[0];
-	var from = querystring.escape(pos[1]);
-
+	var from = pos[1];
+	while(memoTime[from]<time) {
+		if (stack.length>0) {
+			pos = stack.pop();
+			time = pos[0];
+			from = pos[1];
+		}
+		else {
+			setTimeout(function(){calculateHelper(to, speed, res)}, 100);
+			return;
+		}
+	}
+	from = querystring.escape(from);
 	var options = {
 		host: 'dev.virtualearth.net',
 		path: '/REST/V1/Routes/Transit?wp.0='+from+'&wp.1='+to+'&timeType=Departure&dateTime='+convertTime(time)+'&maxSolns=3&output=json&key=Ar4y4wDSYp3CK2xuOrmYnj_CrI-XcCKR9gekEPPSZUWwH5G7QP-8TAwVSp07TD9T'
 	};
-
+	from = pos[1];
 	var callback = function(response) {
 		var str = '';
 
@@ -134,16 +147,38 @@ var calculateHelper = function(to, speed, res) {
 		});
 
 		response.on('end', function () {
-			var resources = JSON.parse(str).resourceSets[0].resources;
+			if (str==="") return;
+			var resources1 = JSON.parse(str).resourceSets[0];
+			if (resources1 === undefined) return;
+			resources = resources1.resources;
+
 			for (var k = 0; k < resources.length; k++) {
 				var resource = resources[k];
 				var data = resource.routeLegs[0];
 				var items = data.itineraryItems;
 				var instr = items[0].instruction.text;
 				console.log(instr);
+				if (instr.indexOf("Walk")===0) {
+					var i = instr.indexOf("to")+3;
+					var dest = instr.substring(i, instr.length);
+					if (dest === from) {
+						items = items.slice(1, items.length);
+						instr = items[0].instruction.text;
+					}
+				}
+				//console.log("from" + from);
 				if (instr.indexOf("Walk")===0 && memoDir[from].indexOf("Walk")===0)
 					continue;
+				if (items[0].travelDuration===0) {
+					if (items.length === 1) {
+						end = from;
+						res.send(accumulatePath());
+						return;
+					}
+					else return;
 
+				}
+				console.log("duration " + items[0].travelDuration + " steps " + items.length);
 				if (instr.indexOf("Walk")===0) {
 					var duration = items[0].travelDuration;
 					
@@ -154,7 +189,7 @@ var calculateHelper = function(to, speed, res) {
 
 					var scaledTime = duration / speed;
 					var newDepartureTime = time + scaledTime*1000;
-					if (dest in memoTime && memoTime[dest]<newDepartureTime) continue;
+					if (dest in memoTime && memoTime[dest]<=newDepartureTime) continue;
 					memoTime[dest] = newDepartureTime;
 					memoPrev[dest] = src;
 					memoDir[dest] = instr;
@@ -163,16 +198,17 @@ var calculateHelper = function(to, speed, res) {
 				}
 				
 				else {
-					var arriveInstr = items[0].childItineraryItems[1].instructon.text;
+					var arriveInstr = items[0].childItineraryItems[1].instruction.text;
 					// TODO : also find time we leave the bus stop
 					
 					var dest = arriveInstr.substring(8, instr.length);
-					var newDepartureTime = (eval("new " + items[0].childItineraryItems[1].time)).getTime();
-					if (dest in memoTime && memoTime[dest]<newDepartureTime) continue;
+					var tempTime = items[0].childItineraryItems[1].time;
+					var newDepartureTime = (eval("new " + tempTime.substring(1, tempTime.length-1))).getTime();
+					if (dest in memoTime && memoTime[dest]<=newDepartureTime) continue;
 					memoTime[dest] = newDepartureTime;
 					memoPrev[dest] = from;
-					memoDir[dest] = items[0].childItineraryItems[0].instructon.text + "\n" +
-							items[0].instructon.text + "\n" +
+					memoDir[dest] = items[0].childItineraryItems[0].instruction.text + "\n" +
+							items[0].instruction.text + "\n" +
 							arriveInstr;
 					if (dest !== end) 
 						stack.push([newDepartureTime, dest]);
