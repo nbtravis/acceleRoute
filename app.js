b9cc0526c3c1fd48eb3778ec3f51a5b42cbff14a
Nate Travis <nathaniel@cmu-902222.wv.cc.cmu.edu>
2015-02-07 00:01:44 -0500

    allowed cors

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 
 });
