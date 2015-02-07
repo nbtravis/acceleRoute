f042fb787916e142fc83f73b51c2222732bc66ec
Ajay Ravindran <ajax013@gmail.com>
2015-02-07 01:01:56 -0500

    some search history functions

--- acceleRoute/www/js/index.js
+++ acceleRoute/www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var maxHistorySize = 20;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -47,3 +48,72 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0;i<ca.length;i++) {
+        var c = ca[i];
+        while(c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name)==0) return c.substring(name.length, c.length);
+
+    }
+    return "";
+}
+
+function getHistory() {
+    var curr_string = getCookie("searches");
+    if (curr_string=="") return [];
+    else return JSON.parse(curr_string);
+
+}
+
+function addToHistory(search) {
+   
+    history = getHistory();
+    if (history.indexOf(search)==-1) {
+        history = [search].concat(history);
+        if (history.length > maxHistorySize)
+            history = history.slice(0, maxHistorySize);
+        setCookie("searches", JSON.stringify(history), 10);
+    }
+    return;
+
+
+}
+var currLocationString = "";
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            currLocationString = position.coords.latitude
+                + ", " + position.coords.longitude;
+        }
+
+    );
+}
+
+function findRoute() {
+    var S = document.getElementById('pos').value;
+    var T = document.getElementById('dest').value;
+    if (S=="Current Location") {
+        getLocation();
+        S = currLocationString;
+    } else {
+        addToHistory(S);
+    }
+    if (T == "Current Location") {
+        getLocation();
+        T = currLocationString;
+    }
+    else
+        addToHistory(T);
+
+    //TODO : send query to Azure
+}
